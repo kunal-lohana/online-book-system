cb4d40041828a0476e2c5a7293426bbd7df99d54
kunal-lohana <kunal.lohana84@gmail.com>
2020-07-20 21:22:45 +0530

    Updates

---
+++ precache-manifest.439dad2fe5b28fd14cab4639b0be3312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f827f66048b84768015b9fbc495b742d",
+    "url": "/online-book-system/index.html"
+  },
+  {
+    "revision": "402c91d9a82fdf4d54a2",
+    "url": "/online-book-system/static/css/main.efa23619.chunk.css"
+  },
+  {
+    "revision": "916d25bc3e66000223de",
+    "url": "/online-book-system/static/js/2.3c5033ec.chunk.js"
+  },
+  {
+    "revision": "402c91d9a82fdf4d54a2",
+    "url": "/online-book-system/static/js/main.fae530d3.chunk.js"
+  },
+  {
+    "revision": "5c75bc0b8fe0c2fda6d7",
+    "url": "/online-book-system/static/js/runtime~main.e5446169.js"
+  },
+  {
+    "revision": "40a72c241afd59ac49f9065a7046489b",
+    "url": "/online-book-system/static/media/book-image.40a72c24.jpeg"
+  }
+]);
