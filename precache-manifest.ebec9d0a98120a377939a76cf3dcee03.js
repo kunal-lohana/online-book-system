5cd43eec4611a96f4492381f13d50294faec0f4c
kunal-lohana <kunal.lohana84@gmail.com>
2020-08-04 12:17:31 +0530

    Updates

---
+++ precache-manifest.ebec9d0a98120a377939a76cf3dcee03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bb61a48522b0f8c6e95cf3ad93bfbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdff6a2a5bdaae57617f",
+    "url": "/static/css/main.a2477f58.chunk.css"
+  },
+  {
+    "revision": "916d25bc3e66000223de",
+    "url": "/static/js/2.3c5033ec.chunk.js"
+  },
+  {
+    "revision": "5e61a4d584d47b23c74b",
+    "url": "/static/js/3.582ac114.chunk.js"
+  },
+  {
+    "revision": "fdff6a2a5bdaae57617f",
+    "url": "/static/js/main.b64bf077.chunk.js"
+  },
+  {
+    "revision": "151be70841cea6161a28",
+    "url": "/static/js/runtime~main.b0314b81.js"
+  },
+  {
+    "revision": "40a72c241afd59ac49f9065a7046489b",
+    "url": "/static/media/book-image.40a72c24.jpeg"
+  }
+]);
